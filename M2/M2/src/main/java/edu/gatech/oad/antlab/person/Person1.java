a84a0138e0a133cd50d5e79cb923d7fd75968ddf
Solomero, Sharence Rojas <ssolomero@gmail.com>
2017-05-25 20:22:01 -0400

    Changes to Person1

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Sharence Solomero
  *  @version 1.1
  */
 public class Person1 {
@@ -31,7 +31,8 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  int length = input.length();  
+	  return input.substring(length - 2, length) + input.substring(0, length - 2);
 	}
 	
 	/**
