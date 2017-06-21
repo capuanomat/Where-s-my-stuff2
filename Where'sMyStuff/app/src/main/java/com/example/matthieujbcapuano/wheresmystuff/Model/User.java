59166b3d594201e3b15262bf970513cbd2a6c641
capuanomat <capuanomat@gmail.com>
2017-06-21 16:30:49 -0400

    Testing out version control from Android Studio, added comments in User class.

--- Where'sMyStuff/app/src/main/java/com/example/matthieujbcapuano/wheresmystuff/Model/User.java
+++ Where'sMyStuff/app/src/main/java/com/example/matthieujbcapuano/wheresmystuff/Model/User.java
@@ -1,9 +1,5 @@
 package com.example.matthieujbcapuano.wheresmystuff.Model;
 
-/**
- * Created by Matthieu J.B Capuano on 6/20/2017.
- */
-
 public abstract class User {
     /**
      * First three instance variables are strings to define the name of the user, username and
@@ -13,7 +9,18 @@ public abstract class User {
     private String user;
     private String pass;
 
+    /**
+     * Variable to store contact numbers
+     */
     private int[] contactNumbers;
+
+    /**
+     * Boolean variable that tells whether the user has ban permission (is an admin) or not.
+     */
     private boolean banPermission;
+
+    /**
+     * Boolean variable that tells if the user is banned or not.
+     */
     private boolean banned;
 }
