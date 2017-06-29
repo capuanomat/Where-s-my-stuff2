bac98677534a1c6ebd525cb75e23874fcd6c273c
awilkins6 <Aw73511!>
2017-06-29 02:25:31 -0400

    lost items can now be added to lost item page, need to reload page to see updates, current arraylist populating listview is Item instead of LostItem

--- Where'sMyStuff/app/src/main/java/com/example/matthieujbcapuano/wheresmystuff/Controller/LostItemPage.java
+++ Where'sMyStuff/app/src/main/java/com/example/matthieujbcapuano/wheresmystuff/Controller/LostItemPage.java
@@ -3,6 +3,7 @@ package com.example.matthieujbcapuano.wheresmystuff.Controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.matthieujbcapuano.wheresmystuff.Data.LostItemsDB;
@@ -20,6 +23,8 @@ import com.example.matthieujbcapuano.wheresmystuff.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.matthieujbcapuano.wheresmystuff.R.id.lostItems;
+
 //ALEXANDER: Built from a combination M3 and Creating Lists Tutorial online
 public class LostItemPage extends AppCompatActivity {
 
@@ -27,6 +32,7 @@ public class LostItemPage extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private ListView listView;
 
     /** Database variables **/
     private LostItemsDB db2;
@@ -40,7 +46,8 @@ public class LostItemPage extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         /** Alex stuff **/
-        mRecyclerView = (RecyclerView) findViewById(R.id.lostRecyclerView);
+        //mRecyclerView = (RecyclerView) findViewById(R.id.lostRecyclerView);
+        listView = (ListView) findViewById(R.id.lostItems);
 
         /** Database stuff **/
         db2 = new LostItemsDB(this);
@@ -51,6 +58,11 @@ public class LostItemPage extends AppCompatActivity {
         //      LAST LINE WHEREVER YOU NEED THE LATEST LIST
 
         /** Initializing the floating action button and setting it off if pressed */
+
+        ArrayAdapter<Item> arrayAdapter = new ArrayAdapter<Item>(
+                this, android.R.layout.simple_list_item_1, items
+        );
+        listView.setAdapter(arrayAdapter);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,63 +72,65 @@ public class LostItemPage extends AppCompatActivity {
             }
         });
 
-        /** Alex stuff **/
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        addLostItemActivity lostItemManager = new addLostItemActivity();
-        ArrayList<LostItem> list = lostItemManager.getLostItemsList();
-        mAdapter = new MyAdapter(list);
-        mRecyclerView.setAdapter(mAdapter);
-    }
-
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-        private ArrayList<LostItem> mDataset;
-
-        // Provide a reference to the views for each data item
-        // Complex data items may need more than one view per item, and
-        // you provide access to all the views for a data item in a view holder
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            // each data item is just a string in this case
-            public TextView mTextView;
-            public ViewHolder(TextView v) {
-                super(v);
-                mTextView = v;
-            }
-        }
-
-        // Provide a suitable constructor (depends on the kind of dataset)
-        public MyAdapter(ArrayList<LostItem> myDataset) {
-            mDataset = myDataset;
-        }
-
-        // Create new views (invoked by the layout manager)
-        @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                       int viewType) {
-            // create a new view
-            TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.content_lost_item_content, parent, false);
-
-            ViewHolder vh = new ViewHolder(v);
-            return vh;
-        }
-
-        // Replace the contents of a view (invoked by the layout manager)
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            // - get element from your dataset at this position
-            // - replace the contents of the view with that element
-            holder.mTextView.setText(mDataset.get(position).toString());
-
-        }
-
-        // Return the size of your dataset
-        @Override
-        public int getItemCount() {
-            if (mDataset != null) {
-                return mDataset.size();
-            }
-            return 0;
-        }
+//        /** Alex stuff **/
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//        addLostItemActivity lostItemManager = new addLostItemActivity();
+//        //ArrayList<LostItem> list = lostItemManager.getLostItemsList();
+//        mAdapter = new MyAdapter(items);
+//        mRecyclerView.setAdapter(mAdapter);
     }
 }
+
+//    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+//        private List<Item> mDataset;
+//
+//        // Provide a reference to the views for each data item
+//        // Complex data items may need more than one view per item, and
+//        // you provide access to all the views for a data item in a view holder
+//        public class ViewHolder extends RecyclerView.ViewHolder {
+//            // each data item is just a string in this case
+//            public TextView mTextView;
+//            public ViewHolder(TextView v) {
+//                super(v);
+//                mTextView = v;
+//            }
+//        }
+//
+//        // Provide a suitable constructor (depends on the kind of dataset)
+//        public MyAdapter(List<Item> myDataset) {
+//            mDataset = myDataset;
+//        }
+//
+//        // Create new views (invoked by the layout manager)
+//        @Override
+//        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+//                                                       int viewType) {
+//            // create a new view
+////            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+////                    .inflate(R.layout.content_lost_item_page, parent, false);
+//
+////            ViewHolder vh = new ViewHolder(v);
+////            return vh;
+//            return null;
+//        }
+//
+//        // Replace the contents of a view (invoked by the layout manager)
+//        @Override
+//        public void onBindViewHolder(ViewHolder holder, int position) {
+//            // - get element from your dataset at this position
+//            // - replace the contents of the view with that element
+//            holder.mTextView.setText(mDataset.get(position).toString());
+//
+//        }
+//
+//        // Return the size of your dataset
+//        @Override
+//        public int getItemCount() {
+//            if (mDataset != null) {
+//                return mDataset.size();
+//            }
+//            return 0;
+//        }
+//    }
+//}
