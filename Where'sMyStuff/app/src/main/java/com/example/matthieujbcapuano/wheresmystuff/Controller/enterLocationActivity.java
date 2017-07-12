9d45d73efae4218c7d4a0578ffb5b0705f92ffdc
simol80r <simola.nayak@gmail.com>
2017-07-11 22:47:40 -0400

    A few more changes because Android Studio is throwing a fit...
    clean up stray stuff on xml
    add header a bit

--- Where'sMyStuff/app/src/main/java/com/example/matthieujbcapuano/wheresmystuff/Controller/enterLocationActivity.java
+++ Where'sMyStuff/app/src/main/java/com/example/matthieujbcapuano/wheresmystuff/Controller/enterLocationActivity.java
@@ -1,8 +1,11 @@
 package com.example.matthieujbcapuano.wheresmystuff.Controller;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by simolanayak on 7/11/17.
  */
 
-public class enterLocationActivity {
+public class enterLocationActivity extends AppCompatActivity{
+
 }
