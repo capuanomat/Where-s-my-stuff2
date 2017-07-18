15afdd918d0c3f0e5b23a9ef54ba3e050e9a23ad
Solomero, Sharence Rojas <ssolomero@gmail.com>
2017-07-18 18:25:56 -0400

    Method Contract

--- Where'sMyStuff/app/src/test/java/com/example/matthieujbcapuano/wheresmystuff/Model/UserManagerTest.java
+++ Where'sMyStuff/app/src/test/java/com/example/matthieujbcapuano/wheresmystuff/Model/UserManagerTest.java
@@ -8,6 +8,30 @@ import static org.junit.Assert.*;
 
 /**
  * Created by Sharence on 7/15/2017.
+ *
+ * Method Contract:
+ *
+ * findValidUsername is a public method of UserManager class and username is a string that is being
+ * checked to see if someone else has the same username. The method returns true if allUserNames:
+ * ArrayList is empty or does not contain the param; returns false if allUserNames does contain
+ * the param.
+ *
+ * Signature:
+ * + findValidUsername(username: String) : boolean
+ *
+ * Preconditions:
+ * 1) username is of String type
+ * 2) Instance of allUserNames ArrayList with size >= 0
+ *
+ * Postcondition:
+ * 1) Returns true or false of whether Username is valid
+ *
+ * Invariants:
+ * 1) Size of allUserNames >= 0
+ *
+ * Framing Conditions:
+ * 1) If method returns true, the username may be added to the list if password is valid too.
+ 
  */
 public class UserManagerTest {
     @Test
